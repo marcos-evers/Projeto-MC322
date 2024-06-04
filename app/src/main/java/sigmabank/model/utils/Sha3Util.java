1ddd7e9ec6877ca4eb97389000b4a6097e40dc2d
gustavoesteche <gustavoestechea@gmail.com>
2024-06-04 20:49:21 -0300

    Add the utils folder for creating useful tools to be reused alongside the code, sha-3 and HashPassword implementation

---
+++ app/src/main/java/sigmabank/model/utils/Sha3Util.java
@@ -0,0 +1,33 @@
+package sigmabank.model.utils;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+public class Sha3Util {
+    private static final String algorithm = "SHA3-256"; 
+
+    public static String sha3(String input) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance(algorithm);
+            byte[] bytes = input.getBytes(StandardCharsets.UTF_8);
+            byte[] hash = digest.digest(bytes);
+            return bytesToHex(hash);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : bytes) {
+            String hex = Integer.toHexString(b & 0xFF);
+            if (hex.length() == 1) {
+                hexString.append("0");
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+}
