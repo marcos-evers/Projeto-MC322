f2fd2926e2a151748e5ccf778062f7914a9750b1
gustavoesteche <gustavoestechea@gmail.com>
2024-06-12 09:49:50 -0300

    Fix stopped using Date changed to LocalDate
    Since the Date object is deprecated, the date representation changed to LocalDate instead

--- app/src/main/java/sigmabank/model/register/ClientPersonal.java
+++ app/src/main/java/sigmabank/model/register/ClientPersonal.java
@@ -1,11 +1,11 @@
 package sigmabank.model.register;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 import sigmabank.model.card.CreditCard;
 import sigmabank.model.card.DebitCard;
@@ -17,7 +17,7 @@ public class ClientPersonal extends Register {
     private List<CreditCard> creditCards;
     private List<DebitCard> debitCards;
 
-    public ClientPersonal(String name, Date dateOfBirth, String cpf) {
+    public ClientPersonal(String name, LocalDate dateOfBirth, String cpf) {
         super(name, dateOfBirth);
         this.cpf = cpf;
 
