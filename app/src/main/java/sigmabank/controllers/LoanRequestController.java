26699055f93979889aae4a1c3d42b76603f5dd3e
rafomiya <omiyarafael@gmail.com>
2024-06-21 00:40:18 -0300

    add 'loan request' view

---
+++ app/src/main/java/sigmabank/controllers/LoanRequestController.java
@@ -0,0 +1,28 @@
+package sigmabank.controllers;
+
+import java.io.IOException;
+import java.time.format.DateTimeFormatter;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+import sigmabank.model.loan.Loan;
+
+public class LoanRequestController extends BaseController<Loan> {
+    @FXML private Text startDate;
+    @FXML private Text value;
+    @FXML private Text clientUuid;
+
+    @Override
+    public void initData() throws IOException {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        this.startDate.setText(this.object.getStartDay().format(formatter));
+        
+        this.value.setText(this.object.getValue().toString());
+        this.clientUuid.setText(this.object.getClientUUID().toString());
+    }
+
+    public void leave(ActionEvent e) {
+        this.stage.close();
+    }
+}
