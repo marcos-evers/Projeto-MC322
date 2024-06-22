2a7b526eed2434e8ea40d1dbd7f0e8f3a7a81d06
rafomiya <omiyarafael@gmail.com>
2024-06-21 21:09:52 -0300

    implement fancy labels to frequency enum

--- app/src/main/java/sigmabank/model/investment/ROIFrequencyType.java
+++ app/src/main/java/sigmabank/model/investment/ROIFrequencyType.java
@@ -1,8 +1,18 @@
 package sigmabank.model.investment;
 
 public enum ROIFrequencyType {
-    DAILY,
-    WEEKLY,
-    MONTHLY,
-    YEARLY;
+    DAILY("Diário"),
+    WEEKLY("Semanal"),
+    MONTHLY("Mensal"),
+    YEARLY("Anual");
+
+    private final String label;
+
+    private ROIFrequencyType(String label) {
+        this.label = label;
+    }
+
+    public String toString() {
+        return this.label;
+    }
 }
