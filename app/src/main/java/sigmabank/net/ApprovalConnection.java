daea0177f1bda53bb02bdba46d1264c0a17a72f4
Marcos Paulo Evers <marcospauloeversc@gmail.com>
2024-06-22 21:13:01 -0300

    Add Loan Approvement

--- app/src/main/java/sigmabank/net/ApprovalConnection.java
+++ app/src/main/java/sigmabank/net/ApprovalConnection.java
@@ -14,12 +14,13 @@ import java.util.List;
 
 import sigmabank.model.register.Client;
 
-public class ApprovalConnection implements IConnection<Client> {
+public class ApprovalConnection implements IConnection<Object> {
     private final String uri;
 
     public ApprovalConnection(String uri) {
         this.uri = uri;
     }
+
 
     public void send(Map<String, Object> params) throws IOException {
         URL url = URI.create(uri).toURL();
@@ -45,7 +46,7 @@ public class ApprovalConnection implements IConnection<Client> {
         connection.disconnect();
     }
     
-    public List<Client> fetch(Map<String, Object> params) throws IOException {
+    public List<Object> fetch(Map<String, Object> params) throws IOException {
         URL url = URI.create(uri).toURL();
         HttpURLConnection connection = (HttpURLConnection) url.openConnection(); 
 
@@ -58,15 +59,16 @@ public class ApprovalConnection implements IConnection<Client> {
             while ((inputLine = in.readLine()) != null) {
                 response.append(inputLine);
             }
-            // TODO parse Body to list of clients
             System.out.println("[MSG] Response: " + responseCode + "/" + response.toString());
         }
         connection.disconnect();
+        // TODO parse response
         return null;
     }
 
     private String buildPostData(Map<String, Object> params) {
-        return "cpf=" + (String) params.get("cpf") + "&"
-            +   "isapproved=" + Boolean.toString((boolean) params.get("isapproved"));
+        return "type=" + params.get("type") + "&"
+            +   "uuid=" + params.get("uuid").toString() + "&"
+            +   "isapproved=" + params.get("isapproved").toString();
     }
 }
